a9bf5e754f9a35ac9427584f85af801883bdf42c
Pete Albertson <albertson.peter@gmail.com>
2015-09-11 13:55:00 -0700

    various attempts at changing how data is structured and saved

--- build/app.js
+++ build/app.js
@@ -28,30 +28,23 @@
     .controller('MainCtrl', function ($scope, $firebaseArray) {
       var ref = new Firebase('https://pac12survivor.firebaseio.com/');
       var teamArray = $firebaseArray(ref.child('teams'));
+      var gameArray = $firebaseArray(ref.child('games'));
+      var picksRef = ref.child('picks');
       $scope.teams = teamArray;
+      $scope.games = gameArray;
+
 
       $scope.select = function(team, teams, game) {
         if ((!team.chosen && game.available) || game.selected) {
           game.selected = !game.selected;
           team.chosen = !team.chosen;
-          $scope.teams.$save(team, game);
+          picksRef.child(team.$id).set({
+            opponent: game.opponent
+          });
+          // $scope.teams.$save(team, game);
         }
       };
 
-      $scope.column = function(game) {
-        var teamRef = new Firebase("https://pac12survivor.firebaseio.com/teams");
-        teamRef.once("value", function(snapshot) {
-          var teams = snapshot.val();
-          for (var i = 0; i < teams.length; i++) {
-            var gm = teams[i].schedule[game.week - 1]
-            console.log(gm);
-            gm.available = !gm.available;
-            $scope.teams.$save(gm);
-          }
-          // data equals { "name": { "first": "Fred", "last": "Flintstone" }, "age": 53 }
-          // console.log(data);  // 53
-        });
-      }
 
     });
 
