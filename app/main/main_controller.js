940368582322fe55dedec1e2e9ea7ab7349e89e4
Pete Albertson <albertson.peter@gmail.com>
2015-10-17 22:07:31 -0700

    updated controller logic to detach static data from ui

--- app/main/main_controller.js
+++ app/main/main_controller.js
@@ -16,65 +16,96 @@
     })
     .controller('MainCtrl', function ($scope, $firebaseArray) {
       var ref = new Firebase('https://pac12survivor.firebaseio.com/');
-      var teamRef = new Firebase('https://pac12survivor.firebaseio.com/teams');
+      var teamRef = ref.child('teams');
       var teamArray = $firebaseArray(ref.child('teams'));
-      var gameArray = $firebaseArray(ref.child('games'));
-      var user_id = '001';
-      var picksRef = ref.child('picks/' + user_id);
+      var current_user_id = '001';
+      var picksRef = ref.child('picks/' + current_user_id);
       var picksArray = $firebaseArray(picksRef);
       $scope.teams = teamArray;
-      $scope.games = gameArray;
       $scope.picks = picksArray;
-      // console.log($scope.picks);
 
-      // picksRef.on("value", function(snapshot) {
-      //   console.log(snapshot.val());
-      // });
 
-      // $scope.toggleClass = function() {
-      //   if ()
-      // }
+      // is the game's week in the current user's picks table?
+      // if yes, mark games as unavailable
+      // var ref = new Firebase("https://pac12survivor.firebaseio.com/users/picks");
+      var weeksArray = [];
+      picksRef.child("week").on("child_added", function(weeks_snap) {
+        weeksArray.push(weeks_snap.val().week);
+      });
+
+      var teamsArray = [];
+      picksRef.child("team").on("child_added", function(teams_snap) {
+        teamsArray.push(teams_snap.val().team);
+        console.log(teamsArray);
+      });
+
+
+      $scope.pickedTeam = function(team) {
+        var herp = teamsArray.indexOf(team.$id);
+        if (herp != -1) {
+          console.log(herp);
+          return true;
+        }
+
+      };
+
+      $scope.pickedWeek = function(week) {
+        var derp = weeksArray.indexOf(week);
+        if (derp != -1) {
+          console.log(derp);
+          return true;
+        }
+
+      };
+
 
 
       $scope.select = function(team, teams, game) {
         // CHECK TO SEE IF THE GAME IS AVAILABLE OR IF
         // THE TEAM HAS ALREADY BEEN CHOSEN
-        if (!team.chosen && game.available) {
-          game.selected = true;
+        // if (!team.chosen && game.available) {
+          // game.selected = true;
           // team.chosen = true;
           // ADD A PICK TO THE PICKS TABLE
-          picksRef.child(game.week).set({
-            user: user_id,
+          picksRef.child("week").child(game.week).set({
+            user: current_user_id,
             team: team.$id,
             week: game.week
           });
-          $scope.teams.$save(team, game);
+          // $scope.teams.$save(team, game);
+
+          picksRef.child("team").child(team.$id).set({
+            user: current_user_id,
+            team: team.$id,
+            week: game.week
+          });
+          // $scope.teams.$save(team, game);
 
           // LOOP THROUGH OTHER GAMES THAT WEEK AND
           // MAKE THEM UNAVAILABLE
-          for(var i = 0; i < teams.length; i++) {
-            var weekGame = teamRef.child(teams[i].$id + '/schedule/' + (game.week - 1));
-            weekGame.update({
-              "available": false
-            });
-          }
-        } else if (game.selected) {
-          // IF USER IS DESELECTING A GIVEN GAME, RESET
-          // GAME AND DELETE PICK
-          game.selected = false;
-          // team.chosen = false;
-          picksRef.child(game.week).remove();
-          $scope.teams.$save(team, game);
+          // for(var i = 0; i < teams.length; i++) {
+          //   var weekGame = teamRef.child(teams[i].$id + '/schedule/' + (game.week - 1));
+          //   weekGame.update({
+          //     "available": false
+          //   });
+          // }
+        // } else if (game.selected) {
+        //   // IF USER IS DESELECTING A GIVEN GAME, RESET
+        //   // GAME AND DELETE PICK
+        //   game.selected = false;
+        //   // team.chosen = false;
+        //   picksRef.child(game.week).remove();
+        //   $scope.teams.$save(team, game);
 
-          // GO BACK AND MAKE OTHER GAMES FROM THAT WEEK AVAILABLE
-          for(var i = 0; i < teams.length; i++) {
-            var weekGame = teamRef.child(teams[i].$id + '/schedule/' + (game.week - 1));
-            weekGame.update({
-              "available": true
-            });
+        //   // GO BACK AND MAKE OTHER GAMES FROM THAT WEEK AVAILABLE
+        //   for(var y = 0; y < teams.length; y++) {
+        //     var weekGame = teamRef.child(teams[y].$id + '/schedule/' + (game.week - 1));
+        //     weekGame.update({
+        //       "available": true
+        //     });
+        //   }
 
-          }
-        }
+        // }
       };
 
 
